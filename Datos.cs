d992e7ffbf25f2ee5e6a18b09c0afe365a8712ff
Javi-zafiro <javialexiscortez2@gmail.com>
2023-06-20 20:08:02 -0300

    tp7 completo

--- Datos.cs
+++ Datos.cs
@@ -2,30 +2,96 @@ namespace EspacioEmpleados;
 
 public class Empleados
 {
-    private string nombre;
-    private string apellido;
-    private DateTime fechaDeNacimiento;
-    private char estadoCivil;
-    private char genero;
-    private DateTime fechaDeIngreso;
-    private double sueldoBasico;
-
-    public string Nombre { get => nombre; set => nombre = value; }
-    public string Apellido { get => apellido; set => apellido = value; }
-    public DateTime FechaDeNacimiento { get => fechaDeNacimiento; set => fechaDeNacimiento = value; }
-    public char EstadoCivil { get => estadoCivil; set => estadoCivil = value; }
-    public char Genero { get => genero; set => genero = value; }
-    public DateTime FechaDeIngreso { get => fechaDeIngreso; set => fechaDeIngreso = value; }
-    public double SueldoBasico { get => sueldoBasico; set => sueldoBasico = value; }
-
-
-    public enum Cargos{
+     public enum Cargos{
         Auxiliar,
         Administrativo,
         Ingeniero,
         Especialista,
         Investigador
     }
+    private string? nombre;
+    private string? apellido;
+    private DateTime fechaDeNacimiento;
+    private char estadoCivil;
+    private char genero;
+    private DateTime fechaDeIngreso;
+    private double sueldoBasico;
+    private Cargos cargo;
 
-    public Cargos cargo;
+    public string? Nombre { get => nombre; set => nombre = value; }
+    public string? Apellido { get => apellido; set => apellido = value; }
+    public DateTime FechaDeNacimiento { get => fechaDeNacimiento; set => fechaDeNacimiento = value; }
+    public char EstadoCivil { get => estadoCivil; set => estadoCivil = value; }
+    public char Genero { get => genero; set => genero = value; }
+    public DateTime FechaDeIngreso { get => fechaDeIngreso; set => fechaDeIngreso = value; }
+    public double SueldoBasico { get => sueldoBasico; set => sueldoBasico = value; }
+    public Cargos Cargo { get => cargo; set => cargo = value; }
+
+    public int Antiguedad(){
+        DateTime hoy= DateTime.Now;
+        int antiguedad=hoy.Year-FechaDeIngreso.Year;
+        if (hoy.Month < FechaDeIngreso.Month || (hoy.Month == FechaDeIngreso.Month && hoy.Day < FechaDeIngreso.Day))
+                {
+                    antiguedad--;
+                }
+        return antiguedad;
+    }
+    public int Edad(){
+        DateTime hoy= DateTime.Now;
+        int edad=hoy.Year-FechaDeNacimiento.Year;
+        if (hoy.Month < FechaDeNacimiento.Month || (hoy.Month == FechaDeNacimiento.Month && hoy.Day < FechaDeNacimiento.Day))
+                {
+                    edad--;
+                }
+        return edad;
+    }
+
+    public int AniosParaJubilarse(){
+        int jubilacion;
+        int anios;
+        if (Genero=='M')
+        {
+            jubilacion=65;
+        }else
+        {
+            jubilacion=60;
+        }
+        anios=jubilacion-Edad();
+        return anios;
+    }
+    public double Salario()
+    {
+        
+        double adicional = CalcularAdicional();
+        return SueldoBasico + adicional;
+        
+    }
+
+    private double CalcularAdicional()
+    {
+        int antiguedad = Antiguedad();
+        double adicional = 0.0;
+
+        if (antiguedad <= 20)
+        {
+            adicional = SueldoBasico * (antiguedad * 0.01);
+        }
+        else
+        {
+            adicional = SueldoBasico * 0.25;
+        }
+
+        if (Cargo == Cargos.Ingeniero || Cargo == Cargos.Especialista)
+        {
+            adicional *= 1.5;
+        }
+
+        if (EstadoCivil == 'C')
+        {
+            adicional += 15000;
+        }
+
+        return adicional;
+    }
 }
+
--- Program.cs
+++ Program.cs
@@ -1,26 +1,54 @@
 ﻿using EspacioEmpleados;
-DateTime hoy=DateTime.Now;
-double años;
+double montoTotalSalarios = 0.0;
+Empleados empleadoMasProximoJubilarse=null;
+int aniosProximoJubilacion = int.MaxValue;
 
-Empleados prueba = new Empleados();
+Empleados empleado1 = new Empleados();
+empleado1.Nombre = "Juan";
+empleado1.Apellido = "Perez";
+empleado1.FechaDeNacimiento = new DateTime(1985, 10, 15);
+empleado1.EstadoCivil = 'S';
+empleado1.Genero = 'M';
+empleado1.FechaDeIngreso = new DateTime(2010, 5, 1);
+empleado1.SueldoBasico = 65000;
+empleado1.Cargo = Empleados.Cargos.Ingeniero;
 
-prueba.Nombre="javier";
-prueba.Apellido="cortez";
-prueba.FechaDeNacimiento= new DateTime(1999, 3, 26);
-prueba.EstadoCivil='S';
-prueba.Genero='M';
-prueba.FechaDeIngreso= new DateTime(2019, 5, 7);
-prueba.SueldoBasico=26000;
-prueba.cargo=Empleados.Cargos.Investigador;
+Empleados empleado2 = new Empleados();
+empleado2.Nombre = "María";
+empleado2.Apellido = "Lopez";
+empleado2.FechaDeNacimiento = new DateTime(1990, 6, 20);
+empleado2.EstadoCivil = 'C';
+empleado2.Genero = 'F';
+empleado2.FechaDeIngreso = new DateTime(2015, 3, 10);
+empleado2.SueldoBasico = 50000;
+empleado2.Cargo = Empleados.Cargos.Administrativo;
 
-años=hoy.Year-prueba.FechaDeIngreso.Year;
+Empleados empleado3 = new Empleados();
+empleado3.Nombre = "Pedro";
+empleado3.Apellido = "González";
+empleado3.FechaDeNacimiento = new DateTime(1980, 8, 5);
+empleado3.EstadoCivil = 'C';
+empleado3.Genero = 'M';
+empleado3.FechaDeIngreso = new DateTime(2005, 9, 15);
+empleado3.SueldoBasico = 80000;
+empleado3.Cargo = Empleados.Cargos.Especialista;
 
-Console.WriteLine(prueba.Nombre);
-Console.WriteLine(prueba.Apellido);
-Console.WriteLine(prueba.FechaDeNacimiento);
-Console.WriteLine(prueba.EstadoCivil);
-Console.WriteLine(prueba.Genero);
-Console.WriteLine(prueba.FechaDeIngreso);
-Console.WriteLine(prueba.SueldoBasico);
-Console.WriteLine(prueba.cargo);
-Console.WriteLine(años);
+Empleados[] empleados={empleado1, empleado2, empleado3};
+foreach (Empleados empleado in empleados)
+{
+    montoTotalSalarios= montoTotalSalarios + empleado.Salario();
+
+    if (empleado.AniosParaJubilarse() < aniosProximoJubilacion)
+    {
+        empleadoMasProximoJubilarse = empleado;
+        aniosProximoJubilacion = empleado.AniosParaJubilarse();
+    }
+}
+
+Console.WriteLine("Monto Total de Salarios: " + montoTotalSalarios.ToString("C"));
+Console.WriteLine("Empleado más próximo a jubilarse:");
+Console.WriteLine("Nombre: " + empleadoMasProximoJubilarse.Nombre);
+Console.WriteLine("Apellido: " + empleadoMasProximoJubilarse.Apellido);
+Console.WriteLine("Antigüedad: " + empleadoMasProximoJubilarse.Antiguedad());
+Console.WriteLine("Edad: " + empleadoMasProximoJubilarse.Edad());
+Console.WriteLine("Salario: " + empleadoMasProximoJubilarse.Salario().ToString("C"));
